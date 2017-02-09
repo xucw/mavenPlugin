4731c389eca261cfcbe1fafc7b04c4068b75153b
xucw <xucw@fenqi.im>
2017-02-09 10:37:40 +0800

    add new file

---
+++ src/main/java/com/yhd/arch/Hello.java
@@ -0,0 +1,13 @@
+package com.yhd.arch;
+
+public class Hello {
+
+	public String sayHello(String str){
+		return "Hello: "+ str;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
